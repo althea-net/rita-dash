b8ed3e33e08a8ee4faef739054eb8f680524dfe8
ScottWorks <scottworks.io@gmail.com>
2018-02-19 12:47:48 -0500

    removed whitespaces

--- src/App.js
+++ src/App.js
@@ -5,68 +5,70 @@ import Neighbors from "./Neighbors.js";
 import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hash: window.location.hash.substr(1)
-		};
-		this.onHashChange();
-		window.addEventListener("hashchange", this.onHashChange, false);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      hash: window.location.hash.substr(1)
+    };
+    this.onHashChange();
+    window.addEventListener("hashchange", this.onHashChange, false);
+  }
 
-	onHashChange = () => {
-		this.setState({
-			hash: window.location.hash.substr(1)
-		});
-	};
+  onHashChange = () => {
+    this.setState({
+      hash: window.location.hash.substr(1)
+    });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<Navbar color="faded" light expabd="md">
-					<NavbarBrand href="/"> Althea</NavbarBrand>
-					<Nav>
-						<NavItem>
-							<NavLink href="/#wifi-settings">WiFi Settings</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink href="/#payments">Payments</NavLink>
-						</NavItem>
-						<NavItem>
-							<NavLink href="/#neighbors">Neighbors</NavLink>
-						</NavItem>
-					</Nav>
-				</Navbar>
-				<div
-					style={{
-						display: "flex",
-						alignItems: "center",
-						justifyContent: "center"
-					}}>
-					<div
-						style={{
-							width: "100%",
-							maxWidth: 750
-						}}>
-						<Page hash={this.state.hash} />
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Navbar color="faded" light expabd="md">
+          <NavbarBrand href="/"> Althea</NavbarBrand>
+          <Nav>
+            <NavItem>
+              <NavLink href="/#wifi-settings">WiFi Settings</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/#payments">Payments</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/#neighbors">Neighbors</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              maxWidth: 750
+            }}
+          >
+            <Page hash={this.state.hash} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function Page({ hash, changePage }) {
-	switch (hash) {
-		case "wifi-settings":
-			return <div>wifi settings</div>;
-		case "payments":
-			return <Payments />;
-		case "neighbors":
-			return <Neighbors />;
-		default:
-			return <Frontpage changePage={changePage} />;
-	}
+  switch (hash) {
+    case "wifi-settings":
+      return <div>wifi settings</div>;
+    case "payments":
+      return <Payments />;
+    case "neighbors":
+      return <Neighbors />;
+    default:
+      return <Frontpage changePage={changePage} />;
+  }
 }
 
 export default App;
