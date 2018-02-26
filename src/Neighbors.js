7f8078da160926508562a3e77b959fcb0259e0f2
Jehan <jehan.tremback@gmail.com>
2018-02-25 22:06:35 -0800

    tweaks to line display of speed etc

--- src/Neighbors.js
+++ src/Neighbors.js
@@ -21,6 +21,7 @@ const neighbors = [
     name: "CascadianMesh Tower2",
     linkCost: 5,
     routeMetricToExit: 5,
+    priceToExit: 12,
     currentDebt: 104,
     totalDebt: 0
   },
@@ -42,6 +43,7 @@ const neighbors = [
     name: "Donald J. Trump",
     linkCost: 7,
     routeMetricToExit: 5,
+    priceToExit: 8,
     currentDebt: 234,
     totalDebt: 2
   },
@@ -49,6 +51,7 @@ const neighbors = [
     name: "Franklin",
     linkCost: 4,
     routeMetricToExit: 6,
+    priceToExit: 4,
     currentDebt: 0,
     totalDebt: 0
   },
@@ -56,6 +59,7 @@ const neighbors = [
     name: "78oxoxox",
     linkCost: 5,
     routeMetricToExit: 10,
+    priceToExit: 120,
     currentDebt: 0,
     totalDebt: 0
   },
@@ -63,6 +67,7 @@ const neighbors = [
     name: "Jeff Knuckles",
     linkCost: 5,
     routeMetricToExit: 10,
+    priceToExit: 1,
     currentDebt: 0,
     totalDebt: 0
   },
@@ -70,6 +75,7 @@ const neighbors = [
     name: "flopington",
     linkCost: 2,
     routeMetricToExit: 4,
+    priceToExit: 20,
     currentDebt: 0,
     totalDebt: 199
   },
@@ -77,6 +83,7 @@ const neighbors = [
     name: "doonesbury",
     linkCost: 3,
     routeMetricToExit: 2,
+    priceToExit: 5,
     currentDebt: 0,
     totalDebt: 2009
   }
@@ -115,8 +122,24 @@ function LabelUnit({ label, content, marginBottom, marginRight }) {
   );
 }
 
-function ConnectionLine({ label, thickness, children, dash, scroll }) {
-  // if (scroll)
+function ConnectionLine({
+  label,
+  thickness,
+  children,
+  dash,
+  scrollDirection,
+  scrollSpeed
+}) {
+  let animation;
+  if (scrollDirection && scrollSpeed) {
+    if (scrollDirection > 0) {
+      animation = `ScrollLeft ${scrollSpeed}s linear infinite`;
+    } else {
+      animation = `ScrollRight ${scrollSpeed}s linear infinite`;
+    }
+  } else {
+    animation = "none";
+  }
   return (
     <div
       style={{
@@ -132,13 +155,10 @@ function ConnectionLine({ label, thickness, children, dash, scroll }) {
       <div
         style={{
           height: thickness,
-          background: `linear-gradient(90deg, #fff 0%, #fff ${dash /
-            2}%, #000 ${dash / 2}%, #000 ${100 - dash / 2}%, #000 100%)`,
+          background: `linear-gradient(90deg, #fff 0%, #fff ${dash}%, #000 ${dash}%, #000 ${100 -
+            dash}%, #000 100%)`,
           backgroundSize: thickness * 2,
-          animation:
-            scroll && scroll < 0
-              ? `ScrollRight ${30}s linear infinite`
-              : `ScrollLeft ${30}s linear infinite`,
+          animation,
           width: "100%"
         }}
       />
@@ -150,9 +170,18 @@ function NodeInfo({
   name,
   linkCost,
   routeMetricToExit,
+  priceToExit,
+  greatestCurrentDebt,
   currentDebt,
   totalDebt
 }) {
+  const absCurrentDebt = Math.abs(currentDebt);
+
+  const normalizedCurrentDebt =
+    Math.log(Math.abs(absCurrentDebt / greatestCurrentDebt * 10) + 1) /
+    Math.log(11) *
+    10;
+
   return (
     <div
       style={{
@@ -161,11 +190,12 @@ function NodeInfo({
         marginBottom: 30
       }}
     >
-      <h5 style={{ marginBottom: 0, marginRight: 10 }}>Me</h5>
+      <h3 style={{ marginBottom: 0, marginRight: 10 }}>Me</h3>
       <ConnectionLine
-        thickness={10 - linkCost}
-        dash={100}
-        scroll={currentDebt}
+        thickness={10}
+        dash={(linkCost + 1) * 10}
+        scrollDirection={currentDebt}
+        scrollSpeed={(11 - normalizedCurrentDebt) * 10}
       />
       <div>
         <Card
@@ -199,7 +229,12 @@ function NodeInfo({
                 flexWrap: "wrap"
               }}
             >
-              <LabelUnit label="Price" content={`12 ¢/gb`} />
+              {routeMetricToExit < 10 && (
+                <LabelUnit
+                  label="Bandwidth price"
+                  content={`${priceToExit} ¢/gb`}
+                />
+              )}
               {currentDebt < 0 ? (
                 <LabelUnit
                   label="They are paying me"
@@ -220,14 +255,32 @@ function NodeInfo({
           </CardBody>
         </Card>
       </div>
-      <ConnectionLine thickness={10 - routeMetricToExit} />
-      <h5 style={{ marginBottom: 0, marginLeft: 10 }}>🌎</h5>
+      <ConnectionLine
+        thickness={10}
+        dash={(routeMetricToExit + 1) * 10}
+        scrollDirection={currentDebt}
+        scrollSpeed={(11 - normalizedCurrentDebt) * 10}
+      />
+      <h3 style={{ marginBottom: 0, marginLeft: 10 }}>🌎</h3>
     </div>
   );
 }
 
 export default () => {
-  return <div>{neighbors.map(neigh => <NodeInfo {...neigh} />)}</div>;
+  const greatestCurrentDebt = neighbors.reduce((acc, neigh) => {
+    if (Math.abs(neigh.currentDebt) > acc) {
+      return Math.abs(neigh.currentDebt);
+    } else {
+      return acc;
+    }
+  }, 0);
+  return (
+    <div>
+      {neighbors.map(neigh => (
+        <NodeInfo greatestCurrentDebt={greatestCurrentDebt} {...neigh} />
+      ))}
+    </div>
+  );
 };
 
 // The poor man's D3
