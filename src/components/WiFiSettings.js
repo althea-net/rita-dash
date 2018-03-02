09912375ef6a67726617ef5c9dfe7bfae31abe54
ScottWorks <scottworks.io@gmail.com>
2018-03-02 15:22:10 -0500

    added Modal to WifiSettingsForm

--- src/components/WiFiSettings.js
+++ src/components/WiFiSettings.js
@@ -7,8 +7,53 @@ import {
   Form,
   FormGroup,
   Label,
-  Input
+  Input,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from "reactstrap";
+
+class AdvancedSettingsModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      network: this.props.network
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          color="primary"
+          onClick={this.toggle}
+          style={{
+            margin: 10
+          }}
+        >
+          Advanced Settings</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.state.network} Band: WiFi Settings</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <Button color="primary" onClick={this.toggle}>Close</Button>
+        </Modal>
+      </div >
+    )
+  }
+}
+
 
 export default class WiFiSettings extends Component {
   render() {
@@ -82,6 +127,7 @@ function WifiSettingsForm({ network }) {
             >
               Save
             </Button>
+            <AdvancedSettingsModal network={network} />
           </FormGroup>
         </Form>
       </CardBody>
