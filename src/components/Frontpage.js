8fca110af37c5efeea41c4f04caaa705b2f61fb2
Jehan <jehan.tremback@gmail.com>
2018-05-14 19:47:39 -0700

    add icons

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -9,23 +9,26 @@ export default () => (
       flexWrap: "wrap"
     }}
   >
-    <Icon link="#wifi-settings">Wifi Settings</Icon>
-    <Icon link="#payments">Payments</Icon>
-    <Icon link="#neighbors">Neighbors</Icon>
+    <Icon image="/payments.svg" link="#payments">
+      Payments
+    </Icon>
+    <Icon image="/neighbors.svg" link="#neighbors">
+      Neighbors
+    </Icon>
+    <Icon image="/network.svg" link="#wifi-settings">
+      Network Settings
+    </Icon>
+    <Icon image="/router.svg" link="#wifi-settings">
+      Router Settings
+    </Icon>
   </div>
 );
 
-function Icon({ children, link }) {
+function Icon({ children, link, image }) {
   return (
     <div style={{ padding: 50, paddingBottom: 0 }}>
       <a href={link}>
-        <div
-          style={{
-            width: 200,
-            height: 200,
-            background: "blue"
-          }}
-        />
+        <img width={256} height={256} src={image} />
         <h2>{children}</h2>
       </a>
     </div>
