60827dd62e05426915f47b29712f89a593118fcd
Adam Soltys <asoltys@gmail.com>
2018-08-22 15:41:36 -0700

    put forms in cards so they have a border around them

--- src/components/VerifyForm.js
+++ src/components/VerifyForm.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import {
   Alert,
   Button,
+  Card,
+  CardBody,
   Form,
   FormFeedback,
   FormGroup,
@@ -78,65 +80,74 @@ class VerifyForm extends Component {
   render() {
     let { timeout, waiting } = this.state;
 
-    return waiting ? (
-      <div>
-        <h5>Register</h5>
-        <Progress color="success" animated value="50" />
-        <p style={{ marginTop: 10 }} className="text-center">
-          <b>Submitting verification code to exit...</b>
-        </p>
-      </div>
-    ) : (
-      <Form onSubmit={this.onSubmit} className="form-inline">
-        <h5>Register</h5>
-        {timeout ? (
-          <Alert color="warning">
-            Registration timed out possibly due to an invalid code
-          </Alert>
-        ) : (
-          <p>
-            A six-digit registration code is being sent to your email address.
-          </p>
-        )}
-        <FormGroup id="form" className="form-inline">
-          <Label for="code" style={{ marginRight: 5 }}>
-            <b>Code</b>
-          </Label>
-          <Input
-            type="text"
-            name="code"
-            placeholder="Enter 6 digit code"
-            maxLength="6"
-            valid={this.isFieldValid("code")}
-            invalid={!(this.isFieldValid("code") || !this.state.fields.code)}
-            onChange={this.onFieldChange}
-            onBlur={this.onBlur}
-            value={this.state.fields.code || ""}
-          />
-          <FormFeedback invalid="true">
-            Please enter a valid 6-digit code
-          </FormFeedback>
-        </FormGroup>
+    return (
+      <Card>
+        <CardBody>
+          {waiting ? (
+            <div>
+              <h5>Register</h5>
+              <Progress color="success" animated value="50" />
+              <p style={{ marginTop: 10 }} className="text-center">
+                <b>Submitting verification code to exit...</b>
+              </p>
+            </div>
+          ) : (
+            <Form onSubmit={this.onSubmit} className="form-inline">
+              <h5>Register</h5>
+              {timeout ? (
+                <Alert color="warning">
+                  Registration timed out possibly due to an invalid code
+                </Alert>
+              ) : (
+                <p>
+                  A six-digit registration code is being sent to your email
+                  address.
+                </p>
+              )}
+              <FormGroup id="form" className="form-inline">
+                <Label for="code" style={{ marginRight: 5 }}>
+                  <b>Code</b>
+                </Label>
+                <Input
+                  type="text"
+                  name="code"
+                  placeholder="Enter 6 digit code"
+                  maxLength="6"
+                  valid={this.isFieldValid("code")}
+                  invalid={
+                    !(this.isFieldValid("code") || !this.state.fields.code)
+                  }
+                  onChange={this.onFieldChange}
+                  onBlur={this.onBlur}
+                  value={this.state.fields.code || ""}
+                />
+                <FormFeedback invalid="true">
+                  Please enter a valid 6-digit code
+                </FormFeedback>
+              </FormGroup>
 
-        <FormGroup
-          style={{
-            display: "flex",
-            margin: -20,
-            marginTop: 0,
-            padding: 10
-          }}
-        >
-          <Button
-            color={this.isFieldValid("code") ? "primary" : "secondary"}
-            disabled={!this.isFieldValid("code")}
-            style={{
-              margin: 10
-            }}
-          >
-            Verify
-          </Button>
-        </FormGroup>
-      </Form>
+              <FormGroup
+                style={{
+                  display: "flex",
+                  margin: -20,
+                  marginTop: 0,
+                  padding: 10
+                }}
+              >
+                <Button
+                  color={this.isFieldValid("code") ? "primary" : "secondary"}
+                  disabled={!this.isFieldValid("code")}
+                  style={{
+                    margin: 10
+                  }}
+                >
+                  Verify
+                </Button>
+              </FormGroup>
+            </Form>
+          )}
+        </CardBody>
+      </Card>
     );
   }
 }
