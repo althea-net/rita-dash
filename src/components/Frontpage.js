6fc646e98489ea2efdc3fa66888b50212de44496
Adam Soltys <asoltys@gmail.com>
2018-09-04 16:41:13 -0700

    display some basic node info on the front page

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,40 +1,39 @@
-import React from "react";
-import neighbors from "../images/neighbors.svg";
-import network from "../images/network.svg";
-import router from "../images/router.svg";
-import payments from "../images/payments.svg";
+import React, { Component } from "react";
+import { actions, connect } from "../store";
+import QR from "qrcode.react";
 
-export default () => (
-  <div
-    style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      flexWrap: "wrap"
-    }}
-  >
-    <Icon image={neighbors} link="#neighbors">
-      Neighbors
-    </Icon>
-    <Icon image={network} link="#network-settings">
-      Network Settings
-    </Icon>
-    <Icon image={router} link="#router-settings">
-      Router Settings
-    </Icon>
-    <Icon image={payments} link="#payments">
-      Payments
-    </Icon>
-  </div>
-);
+class FrontPage extends Component {
+  componentDidMount() {
+    actions.getInfo();
+    actions.getSettings();
+  }
 
-function Icon({ children, link, image }) {
-  return (
-    <div style={{ padding: 50, paddingBottom: 0 }}>
-      <a href={link}>
-        <img width={256} height={256} src={image} alt="" />
-        <h2>{children}</h2>
-      </a>
-    </div>
-  );
+  render() {
+    let { info, settings } = this.props.state;
+    let { ownIp } = settings.network;
+    let { ethAddress } = settings.payment;
+
+    return (
+      <div>
+        <h1>Welcome to Althea</h1>
+        <p>You are running version {info.version}</p>
+
+        <div>
+          <h2>Node Info</h2>
+          <span>IP address: {ownIp}</span>
+          <br />
+          <span>Ethereum address: {ethAddress}</span>
+          <br />
+          <QR
+            style={{ marginTop: 15 }}
+            size="150"
+            bgcolor="#ff0"
+            value={`althea://dao/add?ip_address=${ownIp}&eth_address=${ethAddress}`}
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+export default connect(["info", "settings"])(FrontPage);
