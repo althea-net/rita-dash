67f387a23734b96fbb1d11c602d0b5ad56f659cb
Adam Soltys <asoltys@gmail.com>
2018-10-01 09:23:08 -0700

    remove store action logging

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/bootstrap-overrides.css";
-import { Provider, subscribe } from "./store";
+import { Provider } from "./store";
 
 import "./i18n";
-
-subscribe((action, state) => {
-  console.log("action:", action, state);
-});
 
 ReactDOM.render(
   <Provider>
