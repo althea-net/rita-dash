b525a3c99e7c541c3e42c102d87f48a78f5a0d58
Adam Soltys <asoltys@gmail.com>
2018-10-19 11:14:04 -0700

    prevent polling requests from executing before previous request finishes

--- src/store/RouterActions.js
+++ src/store/RouterActions.js
@@ -2,9 +2,7 @@ export default backend => {
   return {
     getInterfaces: async ({ setState, state }) => {
       if (state.loadingInterfaces) return;
-
-      if (state.loadingInterfaces === null)
-        setState({ loadingInterfaces: true });
+      setState({ initializing: false, loadingInterfaces: true });
 
       let res = await backend.getInterfaces();
       if (res instanceof Error) {
@@ -44,7 +42,7 @@ export default backend => {
     getWifiSettings: async ({ setState, state }) => {
       if (state.loading) return;
 
-      setState({ loading: true });
+      setState({ initializing: false, loading: true });
 
       let res = await backend.getWifiSettings();
       if (res instanceof Error) {
