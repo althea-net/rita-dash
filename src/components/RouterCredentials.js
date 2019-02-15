4886f0c9537e97179a971700490ac2ffe8c96963
Adam Soltys <asoltys@gmail.com>
2019-02-14 16:33:29 -0800

    router credentials form

--- src/components/RouterCredentials.js
+++ src/components/RouterCredentials.js
@@ -9,49 +9,48 @@ import {
   Input,
   Label
 } from "reactstrap";
-
-import Export from "./Export";
+import { actions } from "../store";
 
 export default ({ balance, symbol }) => {
   let [t] = useTranslation();
-  let [privateKey, setPrivateKey] = useState("");
-  let [exporting, setExporting] = useState(false);
+  let [username, setUsername] = useState("");
+  let [password, setPassword] = useState("");
 
   return (
     <Card className="mb-4">
       <CardBody>
-        <Export
-          exporting={exporting}
-          setExporting={setExporting}
-          privateKey={privateKey}
-        />
-        <Form onSubmit={() => setPrivateKey(privateKey)}>
-          <FormGroup id="form">
-            <h3>{t("privateKeys")}</h3>
-            <p>{t("importKey", { balance, symbol })}</p>
+        <Form onSubmit={() => actions.saveRouterCredentials()}>
+          <h3>{t("routerCredentials")}</h3>
+          <p>{t("theseCredentials")}</p>
 
-            <h4>Import</h4>
-            <Label for="price">{t("privateKeyString")}</Label>
-            <div className="d-flex">
+          <div className="d-flex">
+            <FormGroup className="mb-0">
+              <Label for="username">{t("username")}</Label>
               <Input
+                id="username"
                 className="mr-3"
-                onChange={e => setPrivateKey(e.target.value)}
-                value={privateKey}
-                style={{ width: 350 }}
+                onChange={e => setUsername(e.target.value)}
+                value={username}
+                style={{ width: 250 }}
               />
+            </FormGroup>
+            <FormGroup className="mb-0">
+              <Label for="password">{t("password")}</Label>
+              <Input
+                id="password"
+                type="password"
+                className="mr-3"
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                style={{ width: 250 }}
+              />
+            </FormGroup>
+            <div className="mt-auto">
               <Button color="primary" style={{ width: 100 }} disabled>
-                {t("import")}
+                {t("save")}
               </Button>
             </div>
-          </FormGroup>
-          <h4>{t("export")}</h4>
-          <Button
-            color="secondary"
-            style={{ width: 150 }}
-            onClick={() => setExporting(true)}
-          >
-            {t("export")}
-          </Button>
+          </div>
         </Form>
       </CardBody>
     </Card>
