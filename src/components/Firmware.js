7f610b127c2b1aefb01df135f98f9dde42a3c9fe
Adam Soltys <asoltys@gmail.com>
2019-02-14 18:37:46 -0800

    firmware modal

--- src/components/Firmware.js
+++ src/components/Firmware.js
@@ -1,59 +1,31 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import {
-  Button,
-  Card,
-  CardBody,
-  Form,
-  FormGroup,
-  Input,
-  Label
-} from "reactstrap";
+import { Button, Card, CardBody } from "reactstrap";
+import { connect } from "../store";
 
-import Export from "./Export";
+import UpdateFirmware from "./UpdateFirmware";
 
-export default ({ balance, symbol }) => {
+export default connect(["info"])(({ state: { info } }) => {
   let [t] = useTranslation();
-  let [privateKey, setPrivateKey] = useState("");
-  let [exporting, setExporting] = useState(false);
+  let [checking, setChecking] = useState(false);
+  let { version, ritaVersion } = info;
 
   return (
     <Card className="mb-4">
       <CardBody>
-        <Export
-          exporting={exporting}
-          setExporting={setExporting}
-          privateKey={privateKey}
-        />
-        <Form onSubmit={() => setPrivateKey(privateKey)}>
-          <FormGroup id="form">
-            <h3>{t("privateKeys")}</h3>
-            <p>{t("importKey", { balance, symbol })}</p>
+        <h3>{t("firmware")}</h3>
+        <p>{t("version", { version, ritaVersion })}</p>
 
-            <h4>Import</h4>
-            <Label for="price">{t("privateKeyString")}</Label>
-            <div className="d-flex">
-              <Input
-                className="mr-3"
-                onChange={e => setPrivateKey(e.target.value)}
-                value={privateKey}
-                style={{ width: 350 }}
-              />
-              <Button color="primary" style={{ width: 100 }} disabled>
-                {t("import")}
-              </Button>
-            </div>
-          </FormGroup>
-          <h4>{t("export")}</h4>
-          <Button
-            color="secondary"
-            style={{ width: 150 }}
-            onClick={() => setExporting(true)}
-          >
-            {t("export")}
-          </Button>
-        </Form>
+        <UpdateFirmware open={checking} setOpen={setChecking} />
+
+        <Button
+          color="secondary"
+          style={{ width: 150 }}
+          onClick={() => setChecking(true)}
+        >
+          {t("checkForUpdates")}
+        </Button>
       </CardBody>
     </Card>
   );
-};
+});
