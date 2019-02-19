45cb1cdfc1795ca6ab203d4da8936de5aa6cacd6
Adam Soltys <asoltys@gmail.com>
2019-02-18 16:17:56 -0800

    refactor front page

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,9 +1,7 @@
 import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { Context } from "../store";
-import { Progress } from "reactstrap";
 
-import Error from "./Error";
 import Finances from "./Finances";
 import UsageMetrics from "./UsageMetrics";
 import NetworkConnection from "./NetworkConnection";
@@ -13,30 +11,18 @@ const Frontpage = () => {
   let [t] = useTranslation();
   let {
     state: {
-      error,
-      info: { ritaVersion, version },
-      loading
+      info: { ritaVersion, version }
     }
   } = useContext(Context);
 
   return (
     <>
       <h1>{t("welcome")}</h1>
-
-      {error ? (
-        <Error error={error} />
-      ) : loading ? (
-        <Progress animated color="info" value="100" />
-      ) : (
-        <div>
-          <p>{t("version", { version, ritaVersion })}</p>
-
-          <Finances />
-          <UsageMetrics />
-          <NetworkConnection />
-          <NodeInformation />
-        </div>
-      )}
+      <p>{t("version", { version, ritaVersion })}</p>
+      <Finances />
+      <UsageMetrics />
+      <NetworkConnection />
+      <NodeInformation />
     </>
   );
 };
