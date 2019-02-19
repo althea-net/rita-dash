e67b60a76d65ab0dfde94f8b344dc5f6867e3bae
Adam Soltys <asoltys@gmail.com>
2019-02-18 22:28:05 -0800

    responsive front page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,11 +1,12 @@
 import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Card, CardBody } from "reactstrap";
+
 import Deposit from "./Deposit";
 import Withdraw from "./Withdraw";
-import updown from "../images/up_down.png";
 
+import { Btn, Card, Left, Right } from "./ui";
 import { Context } from "../store";
+import updown from "../images/up_down.png";
 
 export default () => {
   let [t] = useTranslation();
@@ -18,48 +19,30 @@ export default () => {
   } = useContext(Context);
 
   return (
-    <Card className="mb-4">
-      <CardBody className="d-flex">
-        <Deposit open={depositing} setOpen={setDepositing} />
-        <Withdraw open={withdrawing} setOpen={setWithdrawing} />
-        <div
-          className="pr-4 w-50 d-flex flex-column justify-content-between"
-          style={{ borderRight: "1px solid #dadada" }}
-        >
-          <div className="d-flex justify-content-between">
-            <h4>{t("currentBalance")}</h4>
-            <h4>
-              {balance} {symbol}
-            </h4>
-          </div>
-          <div className="d-flex">
-            <Button
-              color="primary"
-              onClick={() => setDepositing(true)}
-              style={{ fontWeight: "bold", width: 150 }}
-            >
-              {t("topUp")}
-            </Button>
-            <Button
-              className="ml-2"
-              color="primary"
-              onClick={() => setWithdrawing(true)}
-              style={{ fontWeight: "bold", width: 150 }}
-            >
-              {t("withdraw")}
-            </Button>
-          </div>
+    <Card>
+      <Deposit open={depositing} setOpen={setDepositing} />
+      <Withdraw open={withdrawing} setOpen={setWithdrawing} />
+      <Left>
+        <div className="d-flex justify-content-between">
+          <h4>{t("currentBalance")}</h4>
+          <h4>
+            {balance} {symbol}
+          </h4>
         </div>
-        <div className="pl-4 w-50 d-flex">
-          <div className="pr-2">
-            <img src={updown} alt="Upload/Download" />
-          </div>
-          <p>
-            Based on your average usage of 8.4 GB per month, your balance will
-            provide you with an estimated <strong>16 weeks</strong> of service.
-          </p>
+        <div className="d-flex justify-content-center">
+          <Btn onClick={() => setDepositing(true)}>{t("topUp")}</Btn>
+          <Btn onClick={() => setWithdrawing(true)}>{t("withdraw")}</Btn>
         </div>
-      </CardBody>
+      </Left>
+      <Right>
+        <div className="pr-2">
+          <img src={updown} alt="Upload/Download" />
+        </div>
+        <p>
+          Based on your average usage of 8.4 GB per month, your balance will
+          provide you with an estimated <strong>16 weeks</strong> of service.
+        </p>
+      </Right>
     </Card>
   );
 };
--- src/components/UsageMetrics.js
+++ src/components/UsageMetrics.js
@@ -1,54 +1,40 @@
 import React from "react";
-import { Card, CardBody } from "reactstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, Heading, Left, Right } from "./ui";
+import { useTranslation } from "react-i18next";
 
 export default () => {
+  let [t] = useTranslation();
+
   return (
     <div style={{ marginBottom: 40 }}>
-      <div className="w-100 d-flex justify-content-between">
-        <h2>Usage Metrics</h2>
-        <div style={{ color: "#3DADF5" }} className="my-auto">
-          <a href="#billing">
-            Review Billing
-            <FontAwesomeIcon
-              size="lg"
-              icon="angle-right"
-              style={{ marginLeft: 10 }}
-            />
-          </a>
-        </div>
-      </div>
-
+      <Heading
+        title={t("usageMetrics")}
+        link="#billing"
+        linkText={t("reviewBilling")}
+      />
       <Card>
-        <CardBody className="d-flex">
-          <div
-            className="pr-4 w-50 d-flex flex-column justify-content-between"
-            style={{ borderRight: "1px solid #dadada" }}
-          >
-            <div className="d-flex flex-wrap justify-content-between">
-              <h5 className="w-100" style={{ color: "gray" }}>
-                Month to Date
-              </h5>
-              <div className="d-flex w-100 justify-content-around">
+        <Left>
+          <div className="d-flex flex-wrap justify-content-between w-100">
+            <h5 style={{ color: "gray" }}>Month to Date</h5>
+            <div className="d-flex w-100 justify-content-around">
+              <div>
                 <div>
-                  <div>
-                    <h4>4.8 GB</h4>
-                  </div>
-                  <div style={{ color: "gray" }}>Usage</div>
+                  <h4>4.8 GB</h4>
                 </div>
+                <div style={{ color: "gray" }}>Usage</div>
+              </div>
+              <div>
                 <div>
-                  <div>
-                    <h4>0.24 ETH</h4>
-                  </div>
-                  <div style={{ color: "gray" }}>Cost</div>
+                  <h4>0.24 ETH</h4>
                 </div>
+                <div style={{ color: "gray" }}>Cost</div>
               </div>
             </div>
           </div>
-          <div className="pl-4 w-50 d-flex flex-wrap">
-            <h5 className="w-100" style={{ color: "gray" }}>
-              Last Month
-            </h5>
+        </Left>
+        <Right>
+          <div className="d-flex flex-wrap justify-content-between w-100">
+            <h5 style={{ color: "gray" }}>Last Month</h5>
             <div className="d-flex w-100 justify-content-around">
               <div>
                 <div>
@@ -64,7 +50,7 @@ export default () => {
               </div>
             </div>
           </div>
-        </CardBody>
+        </Right>
       </Card>
     </div>
   );
