17fc6925161a1d228e538d33b86df2ce6faa4875
Adam Soltys <asoltys@gmail.com>
2019-02-22 13:49:25 -0800

    deposit address copy/paste

--- src/components/Deposit.js
+++ src/components/Deposit.js
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Card, CardBody, Modal, ModalHeader, ModalBody } from "reactstrap";
 import QR from "qrcode.react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Context } from "../store";
 
 export default ({ open, setOpen }) => {
   let [t] = useTranslation();
+  let [copied, setCopied] = useState(false);
   let {
     state: {
       info: { address },
@@ -26,7 +28,14 @@ export default ({ open, setOpen }) => {
         <Card className="mb-4">
           <CardBody className="d-flex">
             <h5 style={{ wordBreak: "break-word" }}>{address}</h5>
-            <FontAwesomeIcon size="lg" icon="copy" style={{ marginLeft: 10 }} />
+            <CopyToClipboard text={address} onCopy={() => setCopied(true)}>
+              <FontAwesomeIcon
+                size="lg"
+                icon="copy"
+                style={{ cursor: "pointer", marginLeft: 10 }}
+              />
+            </CopyToClipboard>
+            {copied && <p className="ml-2">Copied!</p>}
           </CardBody>
         </Card>
         <div className="w-100 text-center">
