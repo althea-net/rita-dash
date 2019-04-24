1d11068ec754dde4f87ba47382d847c27956a05c
Joshua Carr <rrachsoj@gmail.com>
2019-04-23 22:53:27 -0700

    Alternate Payment Link ID

--- src/ui/index.js
+++ src/ui/index.js
@@ -36,7 +36,7 @@ export const Heading = ({ title, link, linkText }) => (
   <div className="w-100 d-flex justify-content-between">
     <h2>{title}</h2>
     <div style={{ color: "#3DADF5" }} className="my-auto">
-      <a href={link}>
+      <a href={link} id="paymentSettingsALT">
         {linkText}
         <FontAwesomeIcon
           size="lg"
