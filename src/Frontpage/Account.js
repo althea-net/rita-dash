b1a8bbf2e6e09621615ac6dc1a021bb1533bedbf
Adam Soltys <asoltys@gmail.com>
2019-04-26 11:48:42 -0700

    calculate monthly bandwidth and estimate weeks of service remaining

--- src/Frontpage/Account.js
+++ src/Frontpage/Account.js
@@ -4,40 +4,82 @@ import { useTranslation } from "react-i18next";
 import Deposit from "../Payments/Deposit";
 import Withdraw from "../Payments/Withdraw";
 
-import { Btn, Card } from "ui";
+import { Btn, Card, Left, Right } from "ui";
 import { toEth } from "utils";
 import AppContext from "store/App";
+import updown from "../images/up_down.png";
+import { get, init } from "store";
+import { BigNumber } from "bignumber.js";
 
 export default () => {
   const [t] = useTranslation();
 
   const [depositing, setDepositing] = useState(false);
   const [withdrawing, setWithdrawing] = useState(false);
+  const [usage, setUsage] = useState([]);
+
   const {
-    info: { balance },
+    info: { balance, localFee },
     symbol
   } = useContext(AppContext);
+
+  init(async () => {
+    setUsage(await get("/usage/client"));
+  });
+
+  const totalUsage = usage.reduce((a, b) => {
+    return a + b.up + b.down;
+  }, 0);
+
+  const weeksPerMonth = 4;
+  const hoursPerDay = 24;
+  const daysPerMonth = 30;
+
+  const avgUsage = totalUsage / usage.length;
+  const perMonthUsageBytes = avgUsage * hoursPerDay * daysPerMonth;
+  const bytesPerGb = BigNumber("1000000000");
+  const perMonthUsage = BigNumber(perMonthUsageBytes.toString())
+    .div(bytesPerGb)
+    .toFixed(0);
+
+  const weeksOfService = BigNumber(balance)
+    .div(localFee)
+    .div(perMonthUsageBytes)
+    .div(weeksPerMonth)
+    .toFixed(0);
 
   return (
     <Card>
       <Deposit open={depositing} setOpen={setDepositing} />
       <Withdraw open={withdrawing} setOpen={setWithdrawing} />
-      <div style={{ paddingLeft: 20 }}>
-        <div className="d-flex justify-content-between">
-          <h4 className="mr-1">{t("currentBalance")}</h4>
-          <h4 id="balance">
-            {toEth(balance)} {symbol}
-          </h4>
+      <Left>
+        <div style={{ paddingLeft: 20 }}>
+          <div className="d-flex justify-content-between">
+            <h4 className="mr-1">{t("currentBalance")}</h4>
+            <h4 id="balance">
+              {toEth(balance)} {symbol}
+            </h4>
+          </div>
+          <div className="d-flex justify-content-center">
+            <Btn id="deposit" onClick={() => setDepositing(true)}>
+              {t("topUp")}
+            </Btn>
+            <Btn id="withdraw" onClick={() => setWithdrawing(true)}>
+              {t("withdraw")}
+            </Btn>
+          </div>
         </div>
-        <div className="d-flex justify-content-center">
-          <Btn id="deposit" onClick={() => setDepositing(true)}>
-            {t("topUp")}
-          </Btn>
-          <Btn id="withdraw" onClick={() => setWithdrawing(true)}>
-            {t("withdraw")}
-          </Btn>
+      </Left>
+      <Right>
+        <div className="pr-2">
+          <img src={updown} alt="Upload/Download" />
         </div>
-      </div>
+        <p>
+          Based on your average usage of {perMonthUsage} GB per month, your
+          balance will provide you with an estimated{" "}
+          <strong>{weeksOfService} weeks</strong> of service.
+        </p>
+      </Right>
     </Card>
   );
 };
