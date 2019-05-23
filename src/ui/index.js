4b3d160eb051de433bc24c253062467a7f1b5842
Adam Soltys <asoltys@gmail.com>
2019-05-22 22:58:00 -0700

    better wrapping of front page links

--- src/ui/index.js
+++ src/ui/index.js
@@ -33,9 +33,9 @@ export const Card = ({ children }) => (
 );
 
 export const Heading = ({ title, link, linkText }) => (
-  <div className="w-100 d-flex justify-content-between">
+  <div className="w-100 d-flex flex-wrap justify-content-between">
     <h2>{title}</h2>
-    <div style={{ color: "#3DADF5" }} className="my-auto">
+    <div style={{ color: "#3DADF5" }} className="my-auto ml-auto text-right">
       <a href={link} id="paymentSettingsALT">
         {linkText}
         <FontAwesomeIcon
