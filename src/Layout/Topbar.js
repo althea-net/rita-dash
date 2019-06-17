5b8e60a76a112fa6e903a3f912f6a53b8b3b73d0
Adam Soltys <asoltys@gmail.com>
2019-06-16 22:10:32 -0700

    new favicon, font and brand

--- src/Layout/Topbar.js
+++ src/Layout/Topbar.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
-import logo from "images/althea.png";
 import AltheaNav from "./Nav";
 import LanguageSelector from "./LanguageSelector";
 import LowBalance from "utils/LowBalance";
-
-const logoStyles = { width: 50, height: 50, marginLeft: 10, marginRight: 20 };
 
 const Topbar = ({ open, setOpen }) => {
   let toggle = () => setOpen(!open);
@@ -19,8 +16,7 @@ const Topbar = ({ open, setOpen }) => {
     >
       <div className="d-flex w-100">
         <NavbarBrand id="althea-home" href="#" className="d-flex my-auto">
-          <img src={logo} alt="Althea Logo" style={logoStyles} />
-          <h1 className="mb-0">Althea</h1>
+          <b style={{ fontSize: 24, marginLeft: 35 }}>Althea</b>
         </NavbarBrand>
         <div className="ml-auto d-none d-lg-block mr-4 my-auto">
           <LanguageSelector />
