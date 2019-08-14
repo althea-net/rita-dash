00f43d495f25637e5ad3b8ac535fc981f4ee6b15
Adam Soltys <asoltys@gmail.com>
2019-08-14 12:41:22 -0700

    deposit modal responsive styles

--- src/Deposit.js
+++ src/Deposit.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Card, CardBody, Modal, ModalHeader, ModalBody } from "reactstrap";
+import { Modal, ModalHeader, ModalBody, Tooltip } from "reactstrap";
 import QR from "qrcode.react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useStore } from "store";
 import { toEth } from "utils";
 
-const qrStyle = { height: "auto", width: 300 };
-const iconStyle = { cursor: "pointer", marginLeft: 10 };
-const addressStyle = { fontSize: 16, wordBreak: "break-word" };
+const qrStyle = { height: "auto", width: "80%" };
 
 const Deposit = ({ open, setOpen }) => {
   const [t] = useTranslation();
   const [copied, setCopied] = useState(false);
+
+  const copy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
 
   const [{ address, debt, lowBalance, symbol }] = useStore();
 
@@ -24,30 +27,38 @@ const Deposit = ({ open, setOpen }) => {
   if (!(address && symbol)) return null;
 
   return (
-    <Modal isOpen={open} centered toggle={() => setOpen(!open)}>
+    <Modal isOpen={open} size="sm" centered toggle={() => setOpen(!open)}>
       <ModalHeader>
         {t("deposit")} {symbol}
       </ModalHeader>
       <ModalBody>
-        <Card className="mb-4">
-          <CardBody className="d-flex flex-wrap">
-            <h5 style={addressStyle} id="walletAddr">
+        <div
+          className="mb-4 shadow-none d-flex flex-wrap"
+          style={{
+            border: "1px solid #ddd",
+            borderRadius: 5,
+            wordWrap: "break-word"
+          }}
+        >
+          <div className="d-flex py-2 px-0 w-100">
+            <div className="col-11" id="walletAddr">
               {address}
-              <CopyToClipboard
-                text={address}
-                onCopy={() => setCopied(true)}
-                id="copyWalletAddr"
-              >
-                <FontAwesomeIcon
-                  icon="copy"
-                  style={iconStyle}
-                  className="mr-2"
-                />
-              </CopyToClipboard>
-              {copied && <span>{t("copied")}</span>}
-            </h5>
-          </CardBody>
-        </Card>
+            </div>
+
+            <Tooltip placement="top" isOpen={copied} target="copy">
+              {t("copied")}
+            </Tooltip>
+            <CopyToClipboard text={address} onCopy={copy}>
+              <FontAwesomeIcon
+                id="copy"
+                icon="copy"
+                color="#999"
+                className="mr-2"
+                style={{ cursor: "pointer" }}
+              />
+            </CopyToClipboard>
+          </div>
+        </div>
         <div className="w-100 text-center mb-4">
           <QR style={qrStyle} value={address} />
         </div>
