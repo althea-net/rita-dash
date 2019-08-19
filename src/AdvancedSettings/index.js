1b324a355978c2caa4d1e7be9c2a02e3cf3b4f95
Adam Soltys <asoltys@gmail.com>
2019-08-19 09:52:52 -0700

    advanced settings warning

--- src/AdvancedSettings/index.js
+++ src/AdvancedSettings/index.js
@@ -7,12 +7,24 @@ import DaoFee from "./DaoFee";
 import Notifications from "./Notifications";
 import NodeInformation from "./NodeInformation";
 
+import exclamation from "images/exclamation.svg";
+
 export default () => {
   let [t] = useTranslation();
 
   return (
     <div>
       <h2 id="advancedPage">{t("advancedSettings")}</h2>
+
+      <div className="d-flex mb-3">
+        <img
+          src={exclamation}
+          alt={t("exclamationMark")}
+          style={{ width: 40, marginTop: -12 }}
+          className="mr-2"
+        />
+        <p>{t("theseSettings")}</p>
+      </div>
 
       <Notifications />
       <div className="d-flex" style={{ justifyContent: "space-evenly" }}>
