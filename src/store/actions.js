c263c20bef847f226a5d44a13b67912cb26e5f28
Adam Soltys <asoltys@gmail.com>
2019-08-21 14:03:17 -0700

    remove logging statement

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,13 +9,10 @@ const symbols = {
 export default (state, action) => {
   const { type, ...data } = action;
   const actions = {
-    blockchain: ({ blockchain }) => {
-      console.log("dispatching blockchain");
-      return {
-        blockchain,
-        symbol: symbols[blockchain]
-      };
-    },
+    blockchain: ({ blockchain }) => ({
+      blockchain,
+      symbol: symbols[blockchain]
+    }),
     channels: ({ channels }) => ({ channels }),
     debt: ({ debts }) => {
       const selectedExit = state.exits.find(e => e.isSelected);
