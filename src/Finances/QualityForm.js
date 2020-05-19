fcca2fed283400f9458632bfe1a27495761340a9
Justin Kilpatrick <justin@althea.net>
2020-05-19 17:50:41 -0400

    Set router factor at most once per second
    This prevents the router factor slider from crashing the device

--- src/Finances/QualityForm.js
+++ src/Finances/QualityForm.js
@@ -1,33 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Input } from "reactstrap";
-import { get, post, useDebounce } from "store";
+import { get, post } from "store";
 import "../styles/slider.css";
 
 export default () => {
+  let now = Date.now();
   let [t] = useTranslation();
-  let [factor, setFactor] = useState(0);
+  // the value we currently have and are displaying to the user
+  let [factor, setFactor] = useState(null);
+  // the value we last saw on the router
+  let [routerFactor, setRouterFactor] = useState(null);
+  // the last time we set the router factor
+  let [lastSetTime, setLastSetTime] = useState(0);
 
   useEffect(() => {
     (async () => {
       try {
         let { metricFactor } = await get("/metric_factor");
         setFactor(metricFactor);
+        setRouterFactor(metricFactor);
       } catch {}
     })();
   }, []);
 
-  let debouncedFactor = useDebounce(factor, 500);
   useEffect(
     () => {
       try {
-        post(`/metric_factor/${factor}`);
+        // if the factor and router factor are not null
+        // and the factor is different than what is on the router
+        // and the last time we set the factor was more than a second ago
+        // then you can finally go and make the request
+        if (
+          factor &&
+          routerFactor &&
+          factor !== routerFactor &&
+          now - lastSetTime > 1000
+        ) {
+          post(`/metric_factor/${factor}`);
+          setLastSetTime(now);
+          setRouterFactor(factor);
+        }
       } catch (e) {
         console.log("caught metric factor", e);
       }
     },
-    [debouncedFactor, factor]
+    [factor, routerFactor, lastSetTime, now]
   );
+
+  // handle the null case
+  let display_factor = 0;
+  if (factor) {
+    display_factor = factor;
+  }
 
   return (
     <>
@@ -39,8 +64,8 @@ export default () => {
             id="priceQuality"
             min={0}
             max={6000}
-            value={factor}
-            onChange={e => setFactor(e.target.value)}
+            value={display_factor}
+            onChange={(e) => setFactor(e.target.value)}
           />
           <div className="d-flex justify-content-between">
             <p style={{ maxWidth: 100, fontSize: 14 }}>
