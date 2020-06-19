1797e864a60dd768c577628c9c88ac66a9dc64fd
Justin Kilpatrick <justin@althea.net>
2020-06-19 17:16:41 -0400

    Modfiy deposit flow for wyre
    this uses the new wyre reservation endpoint and dovetails with changes
    on althea_rs and the operator tools to get a reserved link from the
    backend and faciliate wyre usage.

--- src/Deposit.js
+++ src/Deposit.js
@@ -7,26 +7,262 @@ import {
   ModalBody,
   Tooltip,
   Input,
+  FormGroup,
+  Label,
+  InputGroup,
 } from "reactstrap";
 import QR from "qrcode.react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { get, useStore } from "store";
-import { toEth } from "utils";
+import { get, useStore, post } from "store";
+import { toEth, Flags } from "utils";
+import PhoneInput from "react-phone-number-input";
+import SmartInput from "react-phone-number-input/smart-input";
 
 const qrStyle = { height: "auto", width: "80%" };
+
+async function submit(billing_details, phone, email, setState) {
+  try {
+    await post("/billing_details", billing_details, false);
+    await post(`/phone`, phone);
+    await post(`/email`, email);
+    setState("selectAmount");
+  } catch (e) {}
+}
+
+async function get_wyre_url_and_redirect(amount) {
+  let amountJson = {};
+  amountJson["amount"] = amount;
+  try {
+    let res = await post("/wyre_reservation", amountJson);
+    window.location.replace(res.url);
+  } catch (e) {}
+}
+
+function user_info_forum(
+  billingDetails,
+  setBillingDetails,
+  email,
+  setEmail,
+  phone,
+  setPhone,
+  submit,
+  canEdit,
+  setState
+) {
+  if (!billingDetails) {
+    billingDetails = {};
+    billingDetails["mailing_address"] = {};
+    billingDetails.mailing_address["country"] = "United States";
+  }
+  return (
+    <div style={{ width: "100%" }}>
+      <p>Please review your contact info before continuing.</p>
+      <h5>Contact Info</h5>
+      <forum class="form-inline">
+        <FormGroup style={{ width: "50%" }}>
+          <Label>{"First Name"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={billingDetails.user_first_name}
+            placeholder="First Name"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.user_first_name = e.target.value;
+              setBillingDetails(local);
+            }}
+          />
+        </FormGroup>
+        <FormGroup style={{ width: "50%" }}>
+          <Label>{"Last Name"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={billingDetails.user_last_name}
+            placeholder="Last Name"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.user_last_name = e.target.value;
+              setBillingDetails(local);
+            }}
+          />
+        </FormGroup>
+      </forum>
+      <br />
+      <forum class="form-inline">
+        <FormGroup style={{ width: "50%" }}>
+          <Label>{"Email"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={email}
+            placeholder="User Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup style={{ width: "49%" }} className="mb-1 ml-auto text-right">
+          <Label>{"Phonenumber"}</Label>
+          <PhoneInput
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            country="US"
+            flags={Flags}
+            id="exitPhone"
+            inputComponent={SmartInput}
+            placeholder="User Phonenumber"
+            value={phone}
+            onChange={(p) => setPhone(p)}
+          />
+        </FormGroup>
+      </forum>
+      <br />
+      <h5>Address</h5>
+      <Label>{"Street"}</Label>
+      <InputGroup>
+        <Input
+          disabled={canEdit}
+          type="text"
+          placeholder="Street"
+          defaultValue={billingDetails.mailing_address.street}
+          onChange={(e) => {
+            let local = billingDetails;
+            local.mailing_address.street = e.target.value;
+            setBillingDetails(local);
+          }}
+        />
+      </InputGroup>
+      <forum class="form-inline">
+        <FormGroup style={{ width: "25%" }}>
+          <Label>{"Postal Code"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={billingDetails.mailing_address.postal_code}
+            placeholder="Postal Code"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.mailing_address.postal_code = e.target.value;
+              setBillingDetails(local);
+            }}
+          />
+        </FormGroup>
+        <FormGroup style={{ width: "25%" }}>
+          <Label>{"City"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={billingDetails.mailing_address.city}
+            placeholder="City"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.mailing_address.city = e.target.value;
+              setBillingDetails(local);
+            }}
+          />
+        </FormGroup>
+        <FormGroup style={{ width: "25%" }}>
+          <Label>{"State"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="text"
+            defaultValue={billingDetails.mailing_address.state}
+            placeholder="State"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.mailing_address.state = e.target.value;
+              setBillingDetails(local);
+            }}
+          />
+        </FormGroup>
+        <FormGroup style={{ width: "25%" }}>
+          <Label>{"Country"}</Label>
+          <Input
+            disabled={canEdit}
+            style={{ width: "100%" }}
+            type="select"
+            defaultValue={billingDetails.mailing_address.country}
+            placeholder="Country"
+            onChange={(e) => {
+              let local = billingDetails;
+              local.mailing_address.country = e.target.value;
+              setBillingDetails(local);
+            }}
+          >
+            <option selected value="United States">
+              United States
+            </option>
+            <option value="Canada">Canada</option>
+            <option value="Mexico">Mexico</option>
+            <option value="Nigeria">Nigeria</option>
+            <option value="Ghana">Ghana</option>
+            <option value="Uganda">Uganda</option>
+            <option value="Kenya">Kenya</option>
+            <option value="The Bahamas">The Bahamas</option>
+            <option value="Other">Other</option>
+          </Input>
+        </FormGroup>
+      </forum>
+      <br />
+      <Button
+        color="primary"
+        onClick={(e) => {
+          submit(billingDetails, phone, email, setState);
+        }}
+      >
+        {"Continue"}
+      </Button>
+    </div>
+  );
+}
 
 const Deposit = ({ open, setOpen }) => {
   const [t] = useTranslation();
   const [copied, setCopied] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [depositing, setDepositing] = useState(true);
   const [wyreEnabled, setWyreEnabled] = useState(false);
+  // this is true if we find some set of existing details, this
+  // determines if we start in 'review' or 'edit' mode. As normally
+  // the user will be presented an un-editable view that they can opt to
+  // edit, but if no details have been entered this obviously makes no sense
+  // and they are allowed to edit immediately.
+  const [canEdit, setCanEdit] = useState(false);
   const [operatorDebt, setOperatorDebt] = useState(0);
   const [wyreAccountId, setWyreAccountId] = useState();
-  const [wyreWarningModal, setWyreWarningModal] = useState(false);
+  const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [supportNumber, setSupportNumber] = useState("");
+  const [billingDetails, setBillingDetails] = useState("");
+  // this variable controls our progression through the depositing state
+  // machine, displaying one of several options each time. States are
+  // None: display no modal
+  // buySelect: displays the selection between buying and depositing can progress to QR
+  //            reviewDetails or None
+  // QR: Display the QR code, this only returns to None
+  // reviewDetails: This is where the user views and possibly updates their details
+  //                can return to none or progress to selectAmount
+  // selectAmount:  The user selects the amount they would like to deposit, the user
+  //                will either leave the page to go to wyre or return to None
+  const [modalDisplayState, setModalDisplayState] = useState("");
+
+  const toggle = () => {
+    if (modalDisplayState === "None") {
+      if (wyreEnabled && withdrawChainSymbol === "ETH") {
+        setModalDisplayState("buySelect");
+      } else {
+        setModalDisplayState("QR");
+      }
+    } else {
+      setModalDisplayState("None");
+      setOpen(false);
+    }
+  };
 
   const [
     { address, debt, lowBalance, status, withdrawChainSymbol, symbol },
@@ -44,7 +280,6 @@ const Deposit = ({ open, setOpen }) => {
         setWyreEnabled(wyreEnabled);
         setWyreAccountId(wyreAccountId);
         setSupportNumber(supportNumber);
-        if (wyreEnabled && withdrawChainSymbol === "ETH") setDepositing(false);
       } catch {}
 
       setLoading(false);
@@ -58,9 +293,20 @@ const Deposit = ({ open, setOpen }) => {
       const signal = controller.signal;
 
       (async () => {
+        let billingDetails = await get("/billing_details", false);
         let operatorDebt = await get("/operator_debt");
+        let email = await get("/email");
         let phone = await get("/phone");
+
+        if (billingDetails) {
+          setCanEdit(true);
+        } else {
+          setCanEdit(false);
+        }
         if (!(phone instanceof Error)) setPhone(phone);
+        if (!(email instanceof Error)) setEmail(email);
+        if (!(billingDetails instanceof Error))
+          setBillingDetails(billingDetails);
         if (!(operatorDebt instanceof Error)) setOperatorDebt(operatorDebt);
 
         setLoading(true);
@@ -73,6 +319,14 @@ const Deposit = ({ open, setOpen }) => {
     [getWyreEnabled]
   );
 
+  if ((modalDisplayState === "" || modalDisplayState === "None") && open) {
+    if (wyreEnabled && withdrawChainSymbol === "ETH") {
+      setModalDisplayState("buySelect");
+    } else {
+      setModalDisplayState("QR");
+    }
+  }
+
   let decimals = symbol === "Dai" ? 2 : 4;
   let minDeposit = toEth(debt, decimals) * 2 + toEth(operatorDebt, decimals);
   if (status) minDeposit = Math.max(status.minEth, minDeposit);
@@ -81,61 +335,39 @@ const Deposit = ({ open, setOpen }) => {
 
   if (!(address && withdrawChainSymbol)) return null;
 
-  const toggle = () => {
-    setOpen(!open);
-    if (wyreEnabled && withdrawChainSymbol === "ETH") setDepositing(false);
-  };
-
   const copy = () => {
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
-
-  const updateContact = () => {
-    window.location.href = "#settings";
-    let scroll = () => {
-      let el = document.getElementById("notifications");
-      if (el) el.scrollIntoView({ behavior: "smooth" });
-      else setTimeout(scroll, 100);
-    };
-    scroll();
-  };
-
-  let frontpage;
-  let wyre_deposit_error_page;
-  if (window.isMobile) {
-    frontpage = "althea://";
-    wyre_deposit_error_page = "althea://";
-  } else {
-    frontpage = "http://192.168.10.1";
-    wyre_deposit_error_page = "http://192.168.10.1/";
-  }
 
   let modal_body;
   let modal_header = t("addFunds");
 
   // the modal size, edited for when a message case needs a larger modal
   let size = "sm";
-  if (wyreEnabled && withdrawChainSymbol === "ETH" && !wyreWarningModal) {
+  // this displays the buy or QR code modal, only if wyre is enabled
+  if (
+    wyreEnabled &&
+    withdrawChainSymbol === "ETH" &&
+    modalDisplayState === "buySelect"
+  ) {
     modal_body = (
       <>
         <Button
           color="primary"
           className="w-100 mb-2"
-          onClick={() => setWyreWarningModal(true)}
+          onClick={() => setModalDisplayState("reviewDetails")}
         >
           {t("buy")} ETH
         </Button>
 
-        {depositing || (
-          <Button
-            color="primary"
-            className="w-100 mb-2"
-            onClick={() => setDepositing(true)}
-          >
-            {t("deposit")} {withdrawChainSymbol}
-          </Button>
-        )}
+        <Button
+          color="primary"
+          className="w-100 mb-2"
+          onClick={() => setModalDisplayState("QR")}
+        >
+          {t("deposit")} {withdrawChainSymbol}
+        </Button>
 
         <div>
           {lowBalance &&
@@ -144,47 +376,91 @@ const Deposit = ({ open, setOpen }) => {
         </div>
       </>
     );
-  } else if (wyreEnabled && withdrawChainSymbol === "ETH" && wyreWarningModal) {
+  } else if (
+    wyreEnabled &&
+    withdrawChainSymbol === "ETH" &&
+    modalDisplayState === "reviewDetails"
+  ) {
     size = "lg";
+    modal_header = "Review Contact info";
 
-    modal_body = (
-      <>
-        <div>
-          {phone ? t("wyreNumberHelp") : t("wyreNumberHelpNoExample")}
-          {phone ? <Input id="phone" readOnly value={phone || ""} /> : <></>}
-          {phone ? (
-            <>
-              {t("wyreNumberChange")}
-              <a href="#settings" onClick={updateContact}>
-                {"here"}
-              </a>
-            </>
-          ) : (
-            <></>
-          )}
-        </div>
-        <br />
-        <div>{t("wyreInternational")}</div>
-        <br />
-        <div>{t("wyreSupport", { supportNumber })}</div>
-        <hr />
-        <Button
-          href={
-            "https://pay.sendwyre.com/purchase" +
-            `?dest=${address}` +
-            "&destCurrency=ETH" +
-            `&accountId=${wyreAccountId}` +
-            `&redirectUrl=${frontpage}` +
-            `&failureRedirectUrl=${wyre_deposit_error_page}`
-          }
-          color="primary"
-          className="w-25 mb-2"
-        >
-          {t("continueToWyre")}
-        </Button>
-      </>
+    modal_body = user_info_forum(
+      billingDetails,
+      setBillingDetails,
+      email,
+      setEmail,
+      phone,
+      setPhone,
+      submit,
+      false,
+      setModalDisplayState
     );
-  } else {
+  } else if (
+    wyreEnabled &&
+    withdrawChainSymbol === "ETH" &&
+    modalDisplayState === "selectAmount"
+  ) {
+    size = "md";
+    modal_header = "Select deposit amount";
+    modal_body = (
+      <div>
+        <p>Please select an amount to deposit</p>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button
+            onClick={(e) => {
+              get_wyre_url_and_redirect(10.0);
+            }}
+            outline
+            color="primary"
+          >
+            $10
+          </Button>
+          <Button
+            onClick={(e) => {
+              get_wyre_url_and_redirect(20.0);
+            }}
+            outline
+            color="primary"
+          >
+            $20
+          </Button>
+          <Button
+            onClick={(e) => {
+              get_wyre_url_and_redirect(40.0);
+            }}
+            outline
+            color="primary"
+          >
+            $40
+          </Button>
+          <Button
+            onClick={(e) => {
+              get_wyre_url_and_redirect(60.0);
+            }}
+            outline
+            color="primary"
+          >
+            $60
+          </Button>
+          <Button
+            onClick={(e) => {
+              get_wyre_url_and_redirect(100.0);
+            }}
+            outline
+            color="primary"
+          >
+            $100
+          </Button>
+        </div>
+      </div>
+    );
+  } else if (modalDisplayState === "QR") {
     modal_body = (
       <>
         <div
@@ -229,16 +505,15 @@ const Deposit = ({ open, setOpen }) => {
   return (
     <Modal
       isOpen={open && !loading}
-      // this resets the wyre warning modal flow
+      // this resets the contact details review modal flow
       // the flow goes, depositing true -> false
       // which shows the selection modal if appropriate
-      // if you show the selection modal once wyreWarningModal
+      // if you show the selection modal once contactDetailsReviewModal
       // is set to true it shows the final warning page then
       // the user exits.
       // both warning modal and depositing are reset here
-      onClosed={() => {
-        setWyreWarningModal(false);
-        setDepositing(false);
+      onExit={() => {
+        setOpen(false);
       }}
       size={size}
       centered
--- src/Frontpage/OperatorSetup.js
+++ src/Frontpage/OperatorSetup.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Card } from "../ui";
-import { Button, Input, InputGroup } from "reactstrap";
+import { Button, Input, InputGroup, FormGroup, Label } from "reactstrap";
 import { get, post } from "store";
 import PhoneInput from "react-phone-number-input";
 import SmartInput from "react-phone-number-input/smart-input";
@@ -9,13 +9,21 @@ import { Flags } from "utils";
 
 const OperatorSetup = () => {
   const [shouldDisplay, setShouldDisplay] = useState(null);
-  const [userName, setUserName] = useState(null);
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
   const [email, setEmail] = useState(null);
   const [phone, setPhone] = useState(null);
   const [cpeIP, setCpeIP] = useState(null);
+  // on change is only called when a user selects a different value
+  // since the dropdown may be correct to start users might not mess with
+  // it, so it needs to have a correct starting value.
+  const [country, setCountry] = useState("United States");
+  const [postalCode, setPostalCode] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null);
+  const [street, setStreet] = useState(null);
   const [relayAntennaIPs, setRelayAntennaIPs] = useState(null);
   const [phoneClientAntennaIPs, setPhoneClientAntennaIPs] = useState(null);
-  const [mailingAddress, setMailingAddress] = useState(null);
   const [physicalAddress, setPhysicalAddress] = useState(null);
   const [equipmentDetails, setEquipmentDetails] = useState(null);
 
@@ -43,13 +51,18 @@ const OperatorSetup = () => {
   let submit = async (e) => {
     try {
       let install_details = {};
-      install_details.user_name = userName;
+      install_details.first_name = firstName;
+      install_details.last_name = lastName;
+      install_details.country = country;
+      install_details.state = state;
+      install_details.city = city;
+      install_details.street = street;
+      install_details.postal_code = postalCode;
       install_details.phone = phone;
       install_details.email = email;
       install_details.client_antenna_ip = cpeIP;
       install_details.relay_antennas = relayAntennaIPs;
       install_details.phone_client_antennas = phoneClientAntennaIPs;
-      install_details.mailing_address = mailingAddress;
       install_details.physical_address = physicalAddress;
       install_details.equipment_details = equipmentDetails;
       await post(`/installation_details`, install_details);
@@ -80,82 +93,175 @@ const OperatorSetup = () => {
           {t("dismiss")}
         </a>
       </div>
-      <InputGroup>
-        <Input
-          type="text"
-          defaultValue={userName}
-          placeholder="User Name"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          defaultValue={email}
-          placeholder="User Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </InputGroup>
-      <InputGroup>
-        <PhoneInput
-          country="US"
-          flags={Flags}
-          id="exitPhone"
-          inputComponent={SmartInput}
-          placeholder="User PhoneNumber"
-          value={phone}
-          onChange={(p) => handlePhone(p)}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="CPE IP"
-          onChange={(e) => setCpeIP(e.target.value)}
-          defaultValue={cpeIP}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="Relay antenna IPs comma separated list"
-          onChange={(e) => setRelayAntennaIPs(e.target.value)}
-          defaultValue={relayAntennaIPs}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="Phone antenna IPs comma separated list"
-          onChange={(e) => setPhoneClientAntennaIPs(e.target.value)}
-          defaultValue={phoneClientAntennaIPs}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="Mailing address"
-          onChange={(e) => setMailingAddress(e.target.value)}
-          defaultValue={mailingAddress}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="text"
-          placeholder="Physical address"
-          onChange={(e) => setPhysicalAddress(e.target.value)}
-          defaultValue={physicalAddress}
-        />
-      </InputGroup>
-      <InputGroup>
-        <Input
-          type="textarea"
-          placeholder="Equipment details, short summary of hardware"
-          onChange={(e) => setEquipmentDetails(e.target.value)}
-          defaultValue={equipmentDetails}
-        />
-      </InputGroup>
-      <Button onClick={submit}>submit</Button>
+      <div style={{ width: "100%" }}>
+        <forum class="form-inline">
+          <FormGroup style={{ width: "50%" }}>
+            <Label>{"First Name"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={firstName}
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup style={{ width: "50%" }}>
+            <Label>{"Last Name"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={lastName}
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </FormGroup>
+        </forum>
+        <br />
+        <forum class="form-inline">
+          <FormGroup style={{ width: "50%" }}>
+            <Label>{"Email"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={email}
+              placeholder="User Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup
+            style={{ width: "49%" }}
+            className="mb-1 ml-auto text-right"
+          >
+            <Label>{"Phonenumber"}</Label>
+            <PhoneInput
+              style={{ width: "100%" }}
+              country="US"
+              flags={Flags}
+              id="exitPhone"
+              inputComponent={SmartInput}
+              placeholder="User Phonenumber"
+              value={phone}
+              onChange={(p) => setPhone(p)}
+            />
+          </FormGroup>
+        </forum>
+        <br />
+        <Label>{"Street"}</Label>
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="Street"
+            defaultValue={street}
+            onChange={(e) => setStreet(e.target.value)}
+          />
+        </InputGroup>
+        <forum class="form-inline">
+          <FormGroup style={{ width: "25%" }}>
+            <Label>{"Postal Code"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={postalCode}
+              placeholder="Postal Code"
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup style={{ width: "25%" }}>
+            <Label>{"City"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={city}
+              placeholder="City"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup style={{ width: "25%" }}>
+            <Label>{"State"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="text"
+              defaultValue={state}
+              placeholder="State"
+              onChange={(e) => setState(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup style={{ width: "25%" }}>
+            <Label>{"Country"}</Label>
+            <Input
+              style={{ width: "100%" }}
+              type="select"
+              defaultValue={country}
+              placeholder="Country"
+              onChange={(e) => setCountry(e.target.value)}
+            >
+              <option selected value="United States">
+                United States
+              </option>
+              <option value="Canada">Canada</option>
+              <option value="Mexico">Mexico</option>
+              <option value="Nigeria">Nigeria</option>
+              <option value="Ghana">Ghana</option>
+              <option value="Uganda">Uganda</option>
+              <option value="Kenya">Kenya</option>
+              <option value="The Bahamas">The Bahamas</option>
+              <option value="Other">Other</option>
+            </Input>
+          </FormGroup>
+        </forum>
+        <br />
+        <Label>{"Physical Address (If different from mailing)"}</Label>
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="Physical address"
+            onChange={(e) => setPhysicalAddress(e.target.value)}
+            defaultValue={physicalAddress}
+          />
+        </InputGroup>
+        <br />
+        <Label>{"CPE IP"}</Label>
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="CPE IP"
+            onChange={(e) => setCpeIP(e.target.value)}
+            defaultValue={cpeIP}
+          />
+        </InputGroup>
+        <br />
+        <Label>{"Relay Antenna IPs"}</Label>
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="Relay antenna IPs comma separated list"
+            onChange={(e) => setRelayAntennaIPs(e.target.value)}
+            defaultValue={relayAntennaIPs}
+          />
+        </InputGroup>
+        <br />
+        <Label>{"Phone Antenna IPs"}</Label>
+        <InputGroup>
+          <Input
+            type="text"
+            placeholder="Phone antenna IPs comma separated list"
+            onChange={(e) => setPhoneClientAntennaIPs(e.target.value)}
+            defaultValue={phoneClientAntennaIPs}
+          />
+        </InputGroup>
+        <br />
+        <Label>{"Equipment details"}</Label>
+        <InputGroup>
+          <Input
+            type="textarea"
+            placeholder="Equipment details, short summary of hardware"
+            onChange={(e) => setEquipmentDetails(e.target.value)}
+            defaultValue={equipmentDetails}
+          />
+        </InputGroup>
+        <br />
+        <Button onClick={submit}>submit</Button>
+      </div>
     </Card>
   );
 };
